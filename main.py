bf238e915f07e816dbc35029ad86f827b85933b0
agus grandon <agustina107.grandon@gmail.com>
2026-05-19 13:59:50 -0300

    corregi el main

--- main.py
+++ main.py
@@ -23,26 +23,26 @@ while continuar:
             print("Primero debes ingresar la contraseña")
         else:
             match ingresar_opcion: 
-                    case 2:
-                        validar_seguridad_de_contrasenia = nivel_de_seguridad(contrasenia)
-                        print(validar_seguridad_de_contrasenia)
-                    case 3:
-                        contar_datos = contar_caracteres(contrasenia)
-                        print(contar_datos)
-                    case 4:
-                        buscar_caracter_especial = buscar_caracter(contrasenia)
-                        print(buscar_caracter_especial)
-                    case 5:
-                        mostrar_contraseña_invertida = mostrar_invertida(contrasenia)
-                        print(mostrar_contraseña_invertida)
-                    case 6:
-                        generar_reporte_estadistico = reporte_estadistico(contrasenia)
-                        print(generar_reporte_estadistico)
-                    case 7:
-                        palabra_palindromo = verificar_palindromo(contrasenia)
-                        print(palabra_palindromo)
-                    case 8:
-                        opcion_de_ordenar = menu_opcion_de_orden(contrasenia)
-                        print(opcion_de_ordenar)
-                    case _:
-                        print("La opcion ingresada no es correcta, intentelo nuevamente")
+                case 2:
+                    validar_seguridad_de_contrasenia = nivel_de_seguridad(contrasenia)
+                    print(validar_seguridad_de_contrasenia)
+                case 3:
+                    contar_datos = contar_caracteres(contrasenia)
+                    print(contar_datos)
+                case 4:
+                    buscar_caracter_especial = buscar_caracter(contrasenia)
+                    print(buscar_caracter_especial)
+                case 5:
+                    mostrar_contraseña_invertida = mostrar_invertida(contrasenia)
+                    print(mostrar_contraseña_invertida)
+                case 6:
+                    generar_reporte_estadistico = reporte_estadistico(contrasenia)
+                    print(generar_reporte_estadistico)
+                case 7:
+                    palabra_palindromo = verificar_palindromo(contrasenia)
+                    print(palabra_palindromo)
+                case 8:
+                    opcion_de_ordenar = menu_opcion_de_orden(contrasenia)
+                    print(opcion_de_ordenar)
+                case _:
+                    print("La opcion ingresada no es correcta, intentelo nuevamente")
