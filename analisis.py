fb6e6f386d6024be42056d8900d5f658e41ce64a
agus grandon <agustina107.grandon@gmail.com>
2026-05-19 16:17:27 -0300

    corregi la funcion buscar caracter

--- analisis.py
+++ analisis.py
@@ -41,12 +41,12 @@ def buscar_caracter(contrasenia:str) -> str:
             contador_caracter += 1
             posiciones += [i]
 
-        if contador_caracter == 1:
-            mensaje = f"El caracter aparece {contador_caracter} vez, y se encuentra en la posicion {posiciones}.\n"
-        elif contador_caracter >= 2:
-            mensaje = f"El caracter aparece {contador_caracter} veces, y se encuentra en las posiciones {posiciones}.\n"
-        else:
-            mensaje = f"El caracter ingresado no existe en la contraseña.\n"
+    if contador_caracter == 1:
+        mensaje = f"El caracter aparece {contador_caracter} vez, y se encuentra en la posicion {posiciones}.\n"
+    elif contador_caracter >= 2:
+        mensaje = f"El caracter aparece {contador_caracter} veces, y se encuentra en las posiciones {posiciones}.\n"
+    else:
+        mensaje = f"El caracter ingresado no existe en la contraseña.\n"
 
     return mensaje
 
